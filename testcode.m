8289810857d66dbc49e44565c50846d0506ed21d
VLaplaud-Biophysics <73898263+VLaplaud-Biophysics@users.noreply.github.com>
2020-11-10 10:40:05 +0100

    Deleting only one line

--- testcode.m
+++ testcode.m
@@ -1,5 +1,4 @@
 % this is a test code
 % it has several lines
 % and I want to test if I can select the changes I want to commit
-%so I will make that lines disapear
 % but this one should stay here
