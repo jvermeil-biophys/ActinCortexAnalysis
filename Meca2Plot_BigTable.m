e85c82906726d20a0c03fc8d70d5fb9a7b02cccd
VLaplaud-Biophysics <73898263+VLaplaud-Biophysics@users.noreply.github.com>
2020-11-12 12:46:18 +0100

    Step 1 of per cell plot
    Created a cell array with Cell vs TpsComp for Echad, H0EXP and an ID cell

--- Meca2Plot_BigTable.m
+++ Meca2Plot_BigTable.m
@@ -156,230 +156,229 @@ end
 ymax1 = 0;
 ymax2 = 0;
 
-
-
 %% Plots
-
-    
-for kcond = 1:ncond
-    
-    
-    fprintf(['\n\nFor condition : ' Lab{kcond} '\n'])
-    
-    fprintf(['\nNumber of compressions : ' num2str(length(E0chad{kcond})) '.\nNumber of cells : ' num2str(size(UniqueCellList{kcond},1)) '.\n'])
-    
-    
-    
-    if ncond <=2
-        
-        %%  distribution des Echad + median
-        
-        [counts,edges] = histcounts(E0chad{kcond},'binwidth',2);
-        
-        figure(1)
-        hold on
-        histogram(E0chad{kcond},edges,'facecolor',Col{kcond},'facealpha',0.5)
-        plot([median(E0chad{kcond}) median(E0chad{kcond})],[0 1000],'--','linewidth',1.7,'color',Col{kcond},'handlevisibility','off')
-        ax = gca;
-        
-        ymax1tmp = max(counts)*1.05;
-        
-        ymax1 = max([ymax1 ymax1tmp]);
-        
-        ylim([0 ymax1])
-                
-        %% Distrib Echad avec moyenne pondérée en log
-        
-        
-        EchadWeight = (E0chad{kcond}./EchadCI{kcond}).^2;
-        
-        EchadWeightedMean = 10^(sum(log10(E0chad{kcond}).*EchadWeight)/sum(EchadWeight));
-        
-        EchadWeightedStd = sqrt(sum(((E0chad{kcond}-EchadWeightedMean).^2.*EchadWeight))/sum(EchadWeight));
-        
-        
-        [counts,edges] = histcounts(log10(E0chad{kcond}));
-        
-        
-        figure(2)
-        hold on
-        histogram(E0chad{kcond},10.^edges,'facecolor',Col{kcond},'facealpha',0.5)
-        
-        plot([EchadWeightedMean EchadWeightedMean],[0 1000],'--','linewidth',1.7,'color',Col{kcond},'handlevisibility','off')
-        ax = gca;
-        ax.LineWidth = 1.5;
-        ax.XScale = 'log';
-        ax.XTick = [1 10 100];
-        box on
-        
-        
-        xlim([0.5 200])
-        
-        
-        ymax3tmp = max(counts)*1.05;
-        
-        ymax2 = max([ymax2 ymax3tmp]);
-        
-        ylim([0 ymax2])        
-        
-        %% legends & other additions
-        
-        L1.String{end} = [Lab{kcond}  ' - ' num2str(median(E0chad{kcond})) 'kPa'];
-        
-        L2.String{end} = [Lab{kcond}  ' - ' num2str(EchadWeightedMean) '+- ' num2str(EchadWeightedStd) ' kPa'];
-        
-    end
-    
-    
-    %% Module vs Epaisseur départ
-    
-    figure(3)
-    hold on
-    plot(H0EXP{kcond},E0chad{kcond},'ok','markerfacecolor',Col{kcond})
-    
-    if ncond == 2
-        ratioEH{kcond} = (E0chad{kcond}./H0EXP{kcond})/median(E0chad{kcond});
-    end
-    
-    ax = gca;
-    ax.YScale = 'log';
-    ax.XTickLabelRotation = 45;
-    
-    L3.String{end} = Lab{kcond};
-    
-        
-    %% Epaisseur départ vs Compression number
-    
-    figure(7)
-    hold on
-    plot(CompNums{kcond},E0chad{kcond},'ok','markerfacecolor',Col{kcond})
-%    
-    ax = gca;
-%     ax.Yscale = 'log';
-
-    ax.XTickLabelRotation = 45;
-    L7.String{end} = Lab{kcond};
-end
-
-
-    
-    
-    
-    %% Beeswarm plot
-    
-    % all data Echad
-    figure(4)
-    ax = gca;
-    ax.YColor = [0 0 0];
-    ax.LineWidth = 1.5;
-    box on
-    
-    plotSpread_V(E0chad,'distributionMarkers',{'o'},'distributionColors',Col,'xNames',Lab,'spreadWidth',1)
-    for ii = 1:length(E0chad)
-        plot([ii-0.45 ii+0.45],[median(E0chad{ii})  median(E0chad{ii})],'k--','linewidth',1.3)
-    end
-    ylabel('Echad (kPa)')
-    
-    
-    if SigDisplay
-    
-    distsize = prctile(vertcat(E0chad{:}),98);
-    plotheight = distsize;
-    
-    for ii = 1:length(E0chad)-1
-        
-        for jj = ii+1:length(E0chad)
-            
-            sigtxt = DoParamStats(E0chad{ii},E0chad{jj});
-            
-            if ~strcmp(sigtxt,'NS')
-                
-                plot([ii+0.05 jj-0.05],[plotheight plotheight],'k-','linewidth',1.5)
-                text((ii+jj)/2, plotheight + 0.015*distsize, sigtxt,'HorizontalAlignment','center','fontsize',13)
-                
-                plotheight = plotheight + 0.05*distsize;
-            end
-        end
-    end
-    
-     ylim([0 plotheight])
-    
-    end
-    
-     
-    ax = gca;
-    ax.XTickLabelRotation = 45;
-    
-    % H0 
-    
-    
-    H0Beeswarm = H0BEF;
-    
-    figure(6)
-    ax = gca;
-    ax.YColor = [0 0 0];
-    ax.LineWidth = 1.5;
-    box on
-    
-    plotSpread_V(H0Beeswarm,'distributionMarkers',{'o'},'distributionColors',Col,'xNames',Lab,'spreadWidth',1)
-    for ii = 1:length(H0Beeswarm)
-        plot([ii-0.45 ii+0.45],[median(H0Beeswarm{ii})  median(H0Beeswarm{ii})],'k--','linewidth',1.3)
-    end
-    ylabel('H0 pre-ramp')
-    
-    
-    if SigDisplay
-    
-    distsize = prctile(vertcat(H0Beeswarm{:}),90);
-    plotheight = distsize;
-    
-    for ii = 1:length(E0chad)-1
-        for jj = ii+1:length(H0Beeswarm)
-            
-            sigtxt = DoParamStats(H0Beeswarm{ii},H0Beeswarm{jj});
-            
-            if ~strcmp(sigtxt,'NS')
-                
-                plot([ii+0.05 jj-0.05],[plotheight plotheight],'k-','linewidth',1.5)
-                text((ii+jj)/2, plotheight + 0.015*distsize, sigtxt,'HorizontalAlignment','center','fontsize',13)
-                
-                plotheight = plotheight+ 0.05*distsize;
-            end
-            
-        end
-    end
-    
-    ylim([0 plotheight])
-    
-    end
-    ax = gca;
-    ax.XTickLabelRotation = 45;
-    
-    
-    %% per cell plot vs tps comp
 % 
-% if ismember('TpsComp',Cond)
 %     
-%     ptrtpscmp = find(ismember(Cond(1,:),'TpsComp'))+1;
+% for kcond = 1:ncond
 %     
-%     TpsCmpList = Cond(:,ptrtpscmp);
 %     
-%     nTps = length(TpsCmpList)
-%     nCell = length(UniqueCellList);
+%     fprintf(['\n\nFor condition : ' Lab{kcond} '\n'])
 %     
-%     CellVsTime = cell(length(UniqueCellList),nTps);
+%     fprintf(['\nNumber of compressions : ' num2str(length(E0chad{kcond})) '.\nNumber of cells : ' num2str(size(UniqueCellList{kcond},1)) '.\n'])
 %     
-%     for k = 1:nCell
-%         for l = 1:nTps
+%     
+%     
+%     if ncond <=2
+%         
+%         %%  distribution des Echad + median
+%         
+%         [counts,edges] = histcounts(E0chad{kcond},'binwidth',2);
+%         
+%         figure(1)
+%         hold on
+%         histogram(E0chad{kcond},edges,'facecolor',Col{kcond},'facealpha',0.5)
+%         plot([median(E0chad{kcond}) median(E0chad{kcond})],[0 1000],'--','linewidth',1.7,'color',Col{kcond},'handlevisibility','off')
+%         ax = gca;
+%         
+%         ymax1tmp = max(counts)*1.05;
+%         
+%         ymax1 = max([ymax1 ymax1tmp]);
+%         
+%         ylim([0 ymax1])
+%                 
+%         %% Distrib Echad avec moyenne pondérée en log
+%         
+%         
+%         EchadWeight = (E0chad{kcond}./EchadCI{kcond}).^2;
+%         
+%         EchadWeightedMean = 10^(sum(log10(E0chad{kcond}).*EchadWeight)/sum(EchadWeight));
+%         
+%         EchadWeightedStd = sqrt(sum(((E0chad{kcond}-EchadWeightedMean).^2.*EchadWeight))/sum(EchadWeight));
+%         
+%         
+%         [counts,edges] = histcounts(log10(E0chad{kcond}));
+%         
+%         
+%         figure(2)
+%         hold on
+%         histogram(E0chad{kcond},10.^edges,'facecolor',Col{kcond},'facealpha',0.5)
+%         
+%         plot([EchadWeightedMean EchadWeightedMean],[0 1000],'--','linewidth',1.7,'color',Col{kcond},'handlevisibility','off')
+%         ax = gca;
+%         ax.LineWidth = 1.5;
+%         ax.XScale = 'log';
+%         ax.XTick = [1 10 100];
+%         box on
+%         
+%         
+%         xlim([0.5 200])
+%         
+%         
+%         ymax3tmp = max(counts)*1.05;
+%         
+%         ymax2 = max([ymax2 ymax3tmp]);
+%         
+%         ylim([0 ymax2])        
+%         
+%         %% legends & other additions
+%         
+%         L1.String{end} = [Lab{kcond}  ' - ' num2str(median(E0chad{kcond})) 'kPa'];
+%         
+%         L2.String{end} = [Lab{kcond}  ' - ' num2str(EchadWeightedMean) '+- ' num2str(EchadWeightedStd) ' kPa'];
+%         
+%     end
+%     
+%     
+%     %% Module vs Epaisseur départ
+%     
+%     figure(3)
+%     hold on
+%     plot(H0EXP{kcond},E0chad{kcond},'ok','markerfacecolor',Col{kcond})
+%     
+%     if ncond == 2
+%         ratioEH{kcond} = (E0chad{kcond}./H0EXP{kcond})/median(E0chad{kcond});
+%     end
+%     
+%     ax = gca;
+%     ax.YScale = 'log';
+%     ax.XTickLabelRotation = 45;
+%     
+%     L3.String{end} = Lab{kcond};
+%     
+%         
+%     %% Epaisseur départ vs Compression number
+%     
+%     figure(7)
+%     hold on
+%     plot(CompNums{kcond},E0chad{kcond},'ok','markerfacecolor',Col{kcond})
+% %    
+%     ax = gca;
+% %     ax.Yscale = 'log';
+% 
+%     ax.XTickLabelRotation = 45;
+%     L7.String{end} = Lab{kcond};
+% end
+%  
+%% Beeswarm plot
+%     
+%     % all data Echad
+%     figure(4)
+%     ax = gca;
+%     ax.YColor = [0 0 0];
+%     ax.LineWidth = 1.5;
+%     box on
+%     
+%     plotSpread_V(E0chad,'distributionMarkers',{'o'},'distributionColors',Col,'xNames',Lab,'spreadWidth',1)
+%     for ii = 1:length(E0chad)
+%         plot([ii-0.45 ii+0.45],[median(E0chad{ii})  median(E0chad{ii})],'k--','linewidth',1.3)
+%     end
+%     ylabel('Echad (kPa)')
+%     
+%     
+%     if SigDisplay
+%     
+%     distsize = prctile(vertcat(E0chad{:}),98);
+%     plotheight = distsize;
+%     
+%     for ii = 1:length(E0chad)-1
+%         
+%         for jj = ii+1:length(E0chad)
 %             
+%             sigtxt = DoParamStats(E0chad{ii},E0chad{jj});
 %             
+%             if ~strcmp(sigtxt,'NS')
+%                 
+%                 plot([ii+0.05 jj-0.05],[plotheight plotheight],'k-','linewidth',1.5)
+%                 text((ii+jj)/2, plotheight + 0.015*distsize, sigtxt,'HorizontalAlignment','center','fontsize',13)
+%                 
+%                 plotheight = plotheight + 0.05*distsize;
+%             end
+%         end
+%     end
+%     
+%      ylim([0 plotheight])
+%     
+%     end
+%     
+%      
+%     ax = gca;
+%     ax.XTickLabelRotation = 45;
+%     
+%     % H0 
+%     
+%     
+%     H0Beeswarm = H0BEF;
+%     
+%     figure(6)
+%     ax = gca;
+%     ax.YColor = [0 0 0];
+%     ax.LineWidth = 1.5;
+%     box on
+%     
+%     plotSpread_V(H0Beeswarm,'distributionMarkers',{'o'},'distributionColors',Col,'xNames',Lab,'spreadWidth',1)
+%     for ii = 1:length(H0Beeswarm)
+%         plot([ii-0.45 ii+0.45],[median(H0Beeswarm{ii})  median(H0Beeswarm{ii})],'k--','linewidth',1.3)
+%     end
+%     ylabel('H0 pre-ramp')
+%     
+%     
+%     if SigDisplay
+%     
+%     distsize = prctile(vertcat(H0Beeswarm{:}),90);
+%     plotheight = distsize;
+%     
+%     for ii = 1:length(E0chad)-1
+%         for jj = ii+1:length(H0Beeswarm)
 %             
+%             sigtxt = DoParamStats(H0Beeswarm{ii},H0Beeswarm{jj});
+%             
+%             if ~strcmp(sigtxt,'NS')
+%                 
+%                 plot([ii+0.05 jj-0.05],[plotheight plotheight],'k-','linewidth',1.5)
+%                 text((ii+jj)/2, plotheight + 0.015*distsize, sigtxt,'HorizontalAlignment','center','fontsize',13)
+%                 
+%                 plotheight = plotheight+ 0.05*distsize;
+%             end
 %             
 %         end
 %     end
 %     
+%     ylim([0 plotheight])
 %     
-% end
+%     end
+%     ax = gca;
+%     ax.XTickLabelRotation = 45;
+%     
+    
+%% per cell plot vs tps comp
+% 
+if ismember('TpsComp',Cond)
+    
+    ptrtpscmp = find(ismember(Cond(1,:),'TpsComp'))+1;
+    TpsCmpList = Cond(:,ptrtpscmp);
+    
+    FullCellList = unique(vertcat(UniqueCellList{:}));
+    
+    nCell = length(FullCellList);
+    
+    
+    CellVsTpsComp_Echad = cell(ncond,nCell);
+    CellVsTpsComp_H0EXP = cell(ncond,nCell);
+    CellVsTpsComp_ID = cell(ncond,nCell);
+    
+    for kcond = 1:ncond
+        
+        for kc = 1:nCell
+           
+            ptrcell = strcmp(CellIdList{kcond}, FullCellList(kc));
+            
+            CellVsTpsComp_Echad{kcond,kc} = E0chad{kcond}(ptrcell);
+            CellVsTpsComp_H0EXP{kcond,kc} =H0EXP{kcond}(ptrcell);
+            CellVsTpsComp_ID{kcond,kc} = strcat(FullCellList(kc), '-', TpsCmpList(kcond));
+            
+        end        
+    end
+end
 
 
 
