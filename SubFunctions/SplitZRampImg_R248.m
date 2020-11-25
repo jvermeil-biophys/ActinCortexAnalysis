8e95f9a84af6ebaec250f9b28b01a0a5bc897ce1
VLaplaud-Biophysics <vallap.biophysics@protonmail.com>
2020-11-25 18:41:44 +0100

    Commenting some codes used by R2Vs
    - get_fich_names
    - SplitZimage
    - SplitZRampImg
    - SplitZRampImg_R248

--- SubFunctions/SplitZRampImg_R248.m
+++ SubFunctions/SplitZRampImg_R248.m
@@ -1,22 +1,39 @@
 function [Sdown,Smid,Sup,Sramp] = SplitZRampImg_R248(Sfull,nimgbr,nimgar,nimgr1,nimgr2,nimgr3)
 
-% [Sdown,Smid,Sup,Sramp] = SplitZRampImg_R248(Sfull,nimgbr,nimgar,nimgr1,nimgr2,nimgr3)
 %
 % Splits the image list Sfull between list for down, middle and up images
 % corresponding to the triplets taken in Z when not in a ramp.  
-% And a list of image that are during a ramp. Works for ramps of 3
-% different length taken successively
+% And a list of image that are during a ramp. Improvement on SplitZRampImg
+% for the case where there is three different type of ramps.
+%
+%
+% OUTPUTS : 
+% Sdown : list of images that are the first of a Z triplet 
+% Smid : list of images that are the second of a Z triplet 
+% Sup : list of images that are the last of a Z triplet 
+% Sramp : list of images in the ramps
+%
+% INPUTS :
+% Sfull : vector of image numbers to split
+% nimgbr : number of images before a ramp in a loop
+% nimgar : number of images after a ramp in a loop
+% nimgr1: number of images in first ramp
+% nimgr2: number of images in second ramp
+% nimgr3: number of images in third ramp
+%
 
-nimg = 3*nimgbr+nimgr1+3*nimgar+nimgr2+nimgr3;
+nimg = 3*nimgbr+nimgr1+nimgr2+nimgr3+3*nimgar; % total number of image
 
-Mdown = logical(zeros(1,length(Sfull)));
+
+% initializing masks
+Mdown = false(1,length(Sfull));
 Mmid  = Mdown;
 Mup   = Mdown;
 Mramp   = Mdown;
 
 
 
-for i = 1:3:nimgbr-2
+for i = 1:3:nimgbr-2 % before ramp 1
  
     Mdown = Mdown|(mod(Sfull,nimg) == i);
     Mmid = Mmid|(mod(Sfull,nimg) == i+1);
@@ -24,11 +41,11 @@ for i = 1:3:nimgbr-2
 
 end
 
-for i = nimgbr+1:nimgbr+nimgr1
+for i = nimgbr+1:nimgbr+nimgr1 % ramp 1
     Mramp =  Mramp|(mod(Sfull,nimg) == i);
 end
 
-for i = nimgbr+nimgr1+1:3:2*nimgbr+nimgr1+nimgar
+for i = nimgbr+nimgr1+1:3:2*nimgbr+nimgr1+nimgar % between ramp 1 and 2
     
     Mdown = Mdown|(mod(Sfull,nimg) == i);
     Mmid = Mmid|(mod(Sfull,nimg) == i+1);
@@ -37,11 +54,11 @@ for i = nimgbr+nimgr1+1:3:2*nimgbr+nimgr1+nimgar
 end
 
 
-for i = 2*nimgbr+nimgr1+nimgar+1:2*nimgbr+nimgr1+nimgar+nimgr2
+for i = 2*nimgbr+nimgr1+nimgar+1:2*nimgbr+nimgr1+nimgar+nimgr2 % ramp 2
     Mramp =  Mramp|(mod(Sfull,nimg) == i);
 end
 
-for i = 2*nimgbr+nimgr1+nimgar+nimgr2+1:3:3*nimgbr+nimgr1+2*nimgar+nimgr2
+for i = 2*nimgbr+nimgr1+nimgar+nimgr2+1:3:3*nimgbr+nimgr1+2*nimgar+nimgr2 % between ramp 2 and 3
     
     Mdown = Mdown|(mod(Sfull,nimg) == i);
     Mmid = Mmid|(mod(Sfull,nimg) == i+1);
@@ -50,11 +67,11 @@ for i = 2*nimgbr+nimgr1+nimgar+nimgr2+1:3:3*nimgbr+nimgr1+2*nimgar+nimgr2
 end
 
 
-for i = 3*nimgbr+nimgr1+2*nimgar+nimgr2+1:3*nimgbr+nimgr1+2*nimgar+nimgr2+nimgr3
+for i = 3*nimgbr+nimgr1+2*nimgar+nimgr2+1:3*nimgbr+nimgr1+2*nimgar+nimgr2+nimgr3 % ramp 3
     Mramp =  Mramp|(mod(Sfull,nimg) == i);
 end
 
-for i = 3*nimgbr+nimgr1+2*nimgar+nimgr2+nimgr3+1:3:3*nimgbr+nimgr1+3*nimgar+nimgr2+nimgr3-1
+for i = 3*nimgbr+nimgr1+2*nimgar+nimgr2+nimgr3+1:3:3*nimgbr+nimgr1+3*nimgar+nimgr2+nimgr3-1 % after ramp 3
     
     Mdown = Mdown|(mod(Sfull,nimg) == i);
     Mmid = Mmid|(mod(Sfull,nimg) == i+1);
@@ -63,7 +80,7 @@ for i = 3*nimgbr+nimgr1+2*nimgar+nimgr2+nimgr3+1:3:3*nimgbr+nimgr1+3*nimgar+nimg
 end
 
 
-    Mup = Mup|(mod(Sfull,nimg) == 0);
+    Mup = Mup|(mod(Sfull,nimg) == 0); % last image of a loop is always an up image
 
 
 Sdown = Sfull(Mdown);
--- SubFunctions/SplitZimage.m
+++ SubFunctions/SplitZimage.m
@@ -1,35 +1,46 @@
 function [Sdown,Smid,Sup] = SplitZimage(Sfull,nimg)
 
-% [Sdown,Smid,Sup] = SplitZimage(Sfull,nimg)
 %
 % Splits the image list Sfull between list for down, middle and up images
 % corresponding to the triplets taken in Z. nimg is the number of image in
 % a loops and is usefull when fluo images are not present and nimg is not a
 % multiple of 3
+%
+% [Sdown,Smid,Sup] = SplitZimage(Sfull,nimg)
+%
+% OUTPUTS : 
+% Sdown : list of images that are the first of a Z triplet 
+% Smid : list of images that are the second of a Z triplet 
+% Sup : list of images that are the last of a Z triplet 
+%
+% INPUTS :
+% Sfull : vector of image numbers to split
+% nimg : number of images per loop
+%
 
-Mdown = logical(zeros(1,length(Sfull)));
+% initializing masks
+Mdown = false(1,length(Sfull));
 Mmid  = Mdown;
 Mup   = Mdown;
 
 
 
-for i = 1:3:nimg
+for i = 1:3:nimg % going by three for the triplets of images
  
+    % using modulo function to get one image out of three in each list
     Mdown = Mdown|(mod(Sfull,nimg) == i);
     Mmid = Mmid|(mod(Sfull,nimg) == i+1);
-    Mup = Mup|(mod(Sfull,nimg) == i+2);
+    Mup = Mup|(mod(Sfull,nimg) == i+2); % does not work if nimg is multiple of 3
 
 end
 
-if (nimg/3) == fix(nimg/3)
-    Mup = Mup|(mod(Sfull,nimg) == 0);
+if (nimg/3) == fix(nimg/3) % if there is no fluo images (nimg is multiple of 3)
+    Mup = Mup|(mod(Sfull,nimg) == 0); % godd formula for this case
 end
 
+% creating vectors
 Sdown = Sfull(Mdown);
 Smid  = Sfull(Mmid) ;
 Sup   = Sfull(Mup)  ;
-
-
-
 
 end
