3eca8a9221ff7e264f40f610e91fc8eb45164f80
Joseph Vermeil <joseph.vermeil@espci.fr>
2020-12-01 10:33:48 +0100

    Created this script to export data such as V2D or MecaBigTable as .csv

---
+++ ExportMatTablesAsCsv.m
@@ -0,0 +1,7 @@
+% Goal: translate data contained in several .mat files in .csv documents to share with collaborators.
+
+%% V2D tables
+
+dataDir = '';
+fileName = '';
+load([dataDir filesep fileName])
