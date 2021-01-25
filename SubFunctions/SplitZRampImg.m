22e5984d47599876984303fcb0f88b7229371944
Joseph Vermeil <joseph.vermeil@espci.fr>
2021-01-25 20:51:40 +0100

    New version of the SplitZRampImg more convenient to remove black imgs
    Less elegant, more flexible. It loops through the whole list instead of just the nimg first indices, because there can be various nb of black imgs at the end of each loop.

--- SubFunctions/SplitZRampImg.m
+++ SubFunctions/SplitZRampImg.m
@@ -1,4 +1,4 @@
-function [Sdown,Smid,Sup,Sramp,Sfluo] = SplitZRampImg(Sfull,nimgbr,nimgr,nimg,wFluo,nBlackImagesEachLoop)
+function [Sdown,Smid,Sup,Sramp,Sfluo] = SplitZRampImg(Sfull,nimgbr,nimgr,nimg,wFluo,nBlackImagesOfLoopEnd)
 
 %
 % Splits the image list Sfull between list for down, middle and up images
@@ -21,7 +21,9 @@ function [Sdown,Smid,Sup,Sramp,Sfluo] = SplitZRampImg(Sfull,nimgbr,nimgr,nimg,wF
 %
 
 % initializing masks
-Mdown = false(1,length(Sfull));
+N = length(Sfull);
+N_loop = ceil(N/nimg);
+Mdown = false(1,N_loop*nimg);
 Mmid  = Mdown;
 Mup   = Mdown;
 Mramp   = Mdown;
@@ -32,33 +34,60 @@ if wFluo
     nbImgFluo = 1;
 end
     
+for i_loop = 1:N_loop
+    for i = 1:3:nimgbr-2 % going by three for the triplets of images before ramp
+        position = i + (i_loop-1)*nimg;
+        Mdown(position) = true;
+        Mmid(position+1) = true;
+        Mup(position+2) = true;
 
+    end
 
-for i = 1:3:nimgbr-2 % going by three for the triplets of images before ramp
- 
-    Mdown = Mdown|(mod(Sfull,nimg) == i);
-    Mmid = Mmid|(mod(Sfull,nimg) == i+1);
-    Mup = Mup|(mod(Sfull,nimg) == i+2);
+    for i = nimgbr+1 : nimgbr+nimgr - nBlackImagesOfLoopEnd(i_loop) % ramp images
+        position = i + (i_loop-1)*nimg;
+        Mramp(position) = true;
+    end
 
+    for i = nimgbr+nimgr+1 - nBlackImagesOfLoopEnd(i_loop):3:nimg-nbImgFluo - nBlackImagesOfLoopEnd(i_loop) % going by three for the triplets of images after ramp
+        position = i + (i_loop-1)*nimg;
+        Mdown(position) = true;
+        Mmid(position+1) = true;
+        Mup(position+2) = true;
+    end
+
+    %    Mup = Mup|(mod(Sfull,nimg) == 0); % last image of a loop is always an up image
+
+    if wFluo
+        position = nimg - nBlackImagesOfLoopEnd(i_loop) + (i_loop-1)*nimg;
+        Mfluo(position) = true;
+    end
 end
 
-for i = nimgbr+1:nimgbr+nimgr - nBlackImagesEachLoop % ramp images
-    Mramp =  Mramp|(mod(Sfull,nimg) == i);
-end
-
-for i = nimgbr+nimgr+1 - nBlackImagesEachLoop:3: nimg - nbImgFluo - nBlackImagesEachLoop % going by three for the triplets of images after ramp
-    
-    Mdown = Mdown|(mod(Sfull,nimg) == i);
-    Mmid = Mmid|(mod(Sfull,nimg) == i+1);
-    Mup = Mup|(mod(Sfull,nimg) == i+2);
-    
-end
-
-%    Mup = Mup|(mod(Sfull,nimg) == 0); % last image of a loop is always an up image
-
-if wFluo
-    Mfluo = Mfluo|(mod(Sfull+nBlackImagesEachLoop*ones(1,max(Sfull)),nimg) == 0);
-end
+% for i = 1:3:nimgbr-2 % going by three for the triplets of images before ramp
+% 
+%     Mdown = Mdown|(mod(Sfull,nimg) == i);
+%     Mmid = Mmid|(mod(Sfull,nimg) == i+1);
+%     Mup = Mup|(mod(Sfull,nimg) == i+2);
+% 
+% end
+% 
+% for i = nimgbr+1:nimgbr+nimgr - nBlackImagesEachLoop % ramp images
+%     Mramp =  Mramp|(mod(Sfull,nimg) == i);
+% end
+% 
+% for i = nimgbr+nimgr+1 - nBlackImagesEachLoop:3: nimg - nbImgFluo - nBlackImagesEachLoop % going by three for the triplets of images after ramp
+% 
+%     Mdown = Mdown|(mod(Sfull,nimg) == i);
+%     Mmid = Mmid|(mod(Sfull,nimg) == i+1);
+%     Mup = Mup|(mod(Sfull,nimg) == i+2);
+% 
+% end
+% 
+% %    Mup = Mup|(mod(Sfull,nimg) == 0); % last image of a loop is always an up image
+% 
+% if wFluo
+%     Mfluo = Mfluo|(mod(Sfull+nBlackImagesEachLoop*ones(1,max(Sfull)),nimg) == 0);
+% end
 
 Sdown = Sfull(Mdown);
 Smid  = Sfull(Mmid) ;
