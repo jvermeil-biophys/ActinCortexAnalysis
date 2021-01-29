e90ba39787142c45c252adb763c71dbb75c02eb7
VLaplaud-Biophysics <vallap.biophysics@protonmail.com>
2021-01-29 10:32:15 +0100

    Modified indentation figure

--- Old&Other/TestFigPapier.m
+++ Old&Other/TestFigPapier.m
@@ -31,20 +31,22 @@ ForBare = [];
 [ThickBare,ForBare] = Var2Data_Comp('23-01-19','R80',0.6,'M1','DC-Comp_Naked',95,179,'1s',4438,MatfileFolder,FigureFolder,ThickBare,ForBare);
 
 
+ThickBare(:,[33 34 37 38]) = [];
+
 MeanThickCt = mean(ThickCt,2);
-StdThickCt = std(ThickCt-ThickCt(1,:),0,2);
+StdThickCt = std(ThickCt-ThickCt(1,:),0,2)/sqrt(size(ThickCt,2));
 MeanForCt = mean(ForCt,2);
 
 MeanThickLat = mean(ThickLat,2);
-StdThickLat = std(ThickLat-ThickLat(1,:),0,2);
+StdThickLat = std(ThickLat-ThickLat(1,:),0,2)/sqrt(size(ThickLat,2));
 MeanForLat = mean(ForLat,2);
 
 MeanThickOut = mean(ThickOut,2);
-StdThickOut = std(ThickOut-ThickOut(1,:),0,2);
+StdThickOut = std(ThickOut-ThickOut(1,:),0,2)/sqrt(size(ThickOut,2));
 MeanForOut = mean(ForOut,2);
 
 MeanThickBare = mean(ThickBare,2);
-StdThickBare = std(ThickBare-ThickBare(1,:),0,2);
+StdThickBare = std(ThickBare-ThickBare(1,:),0,2)/sqrt(size(ThickBare,2));
 MeanForBare = mean(ForBare,2);
 
 
@@ -53,32 +55,32 @@ MeanForBare = mean(ForBare,2);
 % comp only align
 figure
 hold on
-plot(MeanThickCt(1:47)-MeanThickCt(1),MeanForCt(1:47),'linewidth',7,'color',Cdm)
-plot(MeanThickCt(1:47)-MeanThickCt(1),MeanForCt(1:47)+StdThickCt(1:47),'--','linewidth',1,'color',Cdm)
-plot(MeanThickCt(1:47)-MeanThickCt(1),MeanForCt(1:47)-StdThickCt(1:47),'--','linewidth',1,'color',Cdm)
-fill([MeanThickCt(1:47)-MeanThickCt(1); MeanThickCt(47:-1:1)-MeanThickCt(1); MeanThickCt(1)-MeanThickCt(1)],...
-    [MeanForCt(1:47)+StdThickCt(1:47); MeanForCt(47:-1:1)-StdThickCt(47:-1:1); MeanForCt(1)+StdThickCt(1)],...
+plot(MeanThickCt(1:47)-MeanThickCt(1),MeanForCt(1:47),'linewidth',4,'color',Cdm)
+plot(MeanThickCt(1:47)-MeanThickCt(1)+StdThickCt(1:47),MeanForCt(1:47),'--','linewidth',1,'color',Cdm)
+plot(MeanThickCt(1:47)-MeanThickCt(1)-StdThickCt(1:47),MeanForCt(1:47),'--','linewidth',1,'color',Cdm)
+fill([MeanThickCt(1:47)-MeanThickCt(1)+StdThickCt(1:47); MeanThickCt(47:-1:1)-MeanThickCt(1)-StdThickCt(47:-1:1); MeanThickCt(1)-MeanThickCt(1)+StdThickCt(1)],...
+    [MeanForCt(1:47); MeanForCt(47:-1:1); MeanForCt(1)],...
     Cdm,'facealpha',0.5,'edgecolor','none')
 
-plot(MeanThickLat(1:47)-MeanThickLat(1),MeanForLat(1:47),'linewidth',7,'color',Cl)
-plot(MeanThickLat(1:47)-MeanThickLat(1),MeanForLat(1:47)+StdThickLat(1:47),'--','linewidth',1,'color',Cl)
-plot(MeanThickLat(1:47)-MeanThickLat(1),MeanForLat(1:47)-StdThickLat(1:47),'--','linewidth',1,'color',Cl)
-fill([MeanThickLat(1:47)-MeanThickLat(1); MeanThickLat(47:-1:1)-MeanThickLat(1); MeanThickLat(1)-MeanThickLat(1)],...
-    [MeanForLat(1:47)+StdThickLat(1:47); MeanForLat(47:-1:1)-StdThickLat(47:-1:1); MeanForLat(1)+StdThickLat(1)],...
+plot(MeanThickLat(1:47)-MeanThickLat(1),MeanForLat(1:47),'linewidth',4,'color',Cl)
+plot(MeanThickLat(1:47)-MeanThickLat(1)+StdThickLat(1:47),MeanForLat(1:47),'--','linewidth',1,'color',Cl)
+plot(MeanThickLat(1:47)-MeanThickLat(1)-StdThickLat(1:47),MeanForLat(1:47),'--','linewidth',1,'color',Cl)
+fill([MeanThickLat(1:47)-MeanThickLat(1)+StdThickLat(1:47); MeanThickLat(47:-1:1)-MeanThickLat(1)-StdThickLat(47:-1:1); MeanThickLat(1)-MeanThickLat(1)+StdThickLat(1)],...
+    [MeanForLat(1:47); MeanForLat(47:-1:1); MeanForLat(1)],...
     Cl,'facealpha',0.5,'edgecolor','none')
 
-plot(MeanThickOut(1:47)-MeanThickOut(1),MeanForOut(1:47),'linewidth',7,'color',Co)
-plot(MeanThickOut(1:47)-MeanThickOut(1),MeanForOut(1:47)+StdThickOut(1:47),'--','linewidth',1,'color',Co)
-plot(MeanThickOut(1:47)-MeanThickOut(1),MeanForOut(1:47)-StdThickOut(1:47),'--','linewidth',1,'color',Co)
-fill([MeanThickOut(1:47)-MeanThickOut(1); MeanThickOut(47:-1:1)-MeanThickOut(1); MeanThickOut(1)-MeanThickOut(1)],...
-    [MeanForOut(1:47)+StdThickOut(1:47); MeanForOut(47:-1:1)-StdThickOut(47:-1:1); MeanForOut(1)+StdThickOut(1)],...
+plot(MeanThickOut(1:47)-MeanThickOut(1),MeanForOut(1:47),'linewidth',4,'color',Co)
+plot(MeanThickOut(1:47)-MeanThickOut(1)+StdThickOut(1:47),MeanForOut(1:47),'--','linewidth',1,'color',Co)
+plot(MeanThickOut(1:47)-MeanThickOut(1)-StdThickOut(1:47),MeanForOut(1:47),'--','linewidth',1,'color',Co)
+fill([MeanThickOut(1:47)-MeanThickOut(1)+StdThickOut(1:47); MeanThickOut(47:-1:1)-MeanThickOut(1)-StdThickOut(47:-1:1); MeanThickOut(1)-MeanThickOut(1)+StdThickOut(1)],...
+    [MeanForOut(1:47); MeanForOut(47:-1:1); MeanForOut(1)],...
     Co,'facealpha',0.5,'edgecolor','none')
 
-plot(MeanThickBare(1:47)-MeanThickBare(1),MeanForBare(1:47),'linewidth',7,'color','k')
-plot(MeanThickBare(1:47)-MeanThickBare(1),MeanForBare(1:47)+StdForBare(1:47),'--','linewidth',1,'color','k')
-plot(MeanThickBare(1:47)-MeanThickBare(1),MeanForBare(1:47)-StdForBare(1:47),'--','linewidth',1,'color','k')
-fill([MeanThickBare(1:47)-MeanThickBare(1); MeanThickBare(47:-1:1)-MeanThickBare(1); MeanThickBare(1)-MeanThickBare(1)],...
-    [MeanForBare(1:47)+StdForBare(1:47); MeanForBare(47:-1:1)-StdForBare(47:-1:1); MeanForBare(1)+StdForBare(1)],...
+plot(MeanThickBare(1:47)-MeanThickBare(1),MeanForBare(1:47),'linewidth',4,'color','k')
+plot(MeanThickBare(1:47)-MeanThickBare(1)+StdThickBare(1:47),MeanForBare(1:47),'--','linewidth',1,'color','k')
+plot(MeanThickBare(1:47)-MeanThickBare(1)-StdThickBare(1:47),MeanForBare(1:47),'--','linewidth',1,'color','k')
+fill([MeanThickBare(1:47)-MeanThickBare(1)+StdThickBare(1:47); MeanThickBare(47:-1:1)-MeanThickBare(1)-StdThickBare(47:-1:1); MeanThickBare(1)-MeanThickBare(1)+StdThickBare(1)],...
+    [MeanForBare(1:47); MeanForBare(47:-1:1); MeanForBare(1)],...
     'k','facealpha',0.5,'edgecolor','none')
 
 xlabel('Indentation (nm)')
