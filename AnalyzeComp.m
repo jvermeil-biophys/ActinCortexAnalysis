d1542bf44db12906a92959fa447321db92f6f5e3
Joseph Vermeil <joseph.vermeil@espci.fr>
2021-02-11 18:55:49 +0100

    added the code to load the external experimentalConditions table

--- AnalyzeComp.m
+++ AnalyzeComp.m
@@ -97,6 +97,24 @@ VERBOSED2M = 0; % text display choice
 
 fitparams = 'Strain100-R20.9';
 
+% Load experimental conditions file
+pathExperimentalConditions = [MatfileFolder filesep 'ExperimentalConditions.csv'];
+tableExperimentalConditions = readtable(pathExperimentalConditions,'DatetimeType','text','TextType','string','Delimiter',',','HeaderLines',1);
+if isa(tableExperimentalConditions.magneticFieldCorrection,'string')
+    tableExperimentalConditions.magneticFieldCorrection = str2double(replace(tableExperimentalConditions.magneticFieldCorrection,',','.'));
+end
+if isa(tableExperimentalConditions.scalePixelPerUm,'string')
+    tableExperimentalConditions.scalePixelPerUm = str2double(replace(tableExperimentalConditions.scalePixelPerUm,',','.'));
+end
+if isa(tableExperimentalConditions.opticalIndexCorrection,'string')
+    numbers = split(tableExperimentalConditions.opticalIndexCorrection, '/');
+    if isa(numbers,'string')
+        numbers = replace(numbers,',','.');
+        numbers = str2double(numbers);
+    end
+    tableExperimentalConditions.opticalIndexCorrection = numbers(:,1) ./ numbers(:,2);
+end
+
 
 return % stop execution here
 
