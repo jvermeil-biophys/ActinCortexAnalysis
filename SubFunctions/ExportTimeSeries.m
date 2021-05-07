14bd6f62867b495b92002679d7b94e0e0ade07ad
Joseph Vermeil <joseph.vermeil@espci.fr>
2021-05-07 20:37:49 +0200

    Diverse changes on the python code for ct field analysis
    .txt > .csv & delimeter changes ; more analysis

--- SubFunctions/ExportTimeSeries.m
+++ SubFunctions/ExportTimeSeries.m
@@ -5,5 +5,5 @@ for i=1:nCells
     cellName = MR{i}.name;
     T = table(MR{i}.idxRamp_all, MR{i}.time, MR{i}.B, MR{i}.F, MR{i}.dx, MR{i}.dy, MR{i}.dz, MR{i}.D2, MR{i}.D3,...
             'VariableNames',{'idxCompression','T','B','F','dx','dy','dz','D2','D3'});
-    writetable(T,[ExportDataFolder filesep cellName])
+    writetable(T,[ExportDataFolder filesep cellName '.csv'],'Delimiter',';')
 end
