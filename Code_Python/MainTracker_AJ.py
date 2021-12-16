6f9faa1b4aa853476decc93e632e06657250a555
Anumita <anumitaj1603@gmail.com>
2021-12-16 18:59:42 +0100

    - Added code to clean .txt (.LOG) file obtained from Metamorph on Atchoum containing the times of image acquisition. - Analysed triplets to test the images and depthographs from Atchoum (60X and 100X) using the stand alone version of the main tracker. - Added a small section in the matchDists function to incorporate 'upward' and 'downwards' depthographs.

--- Code_Python/MainTracker_AJ.py
+++ Code_Python/MainTracker_AJ.py
@@ -61,7 +61,7 @@ dateFormatOk = re.compile('\d{2}-\d{2}-\d{2}')
 
 # 5. Add the folder to path
 import sys
-sys.path.append("C://Users//JosephVermeil//Desktop//ActinCortexAnalysis//Code_Python")
+sys.path.append("C://Users//anumi//Desktop//ActinCortexAnalysis//Code_Python")
 
 # %% Import of the BeadTracker functions
 
@@ -74,17 +74,25 @@ rawDataDir = os.path.join(mainDataDir, 'Raw')
 depthoDir = os.path.join(rawDataDir, 'EtalonnageZ')
 interDataDir = os.path.join(mainDataDir, 'Intermediate')
 figureDir = os.path.join(mainDataDir, 'Figures')
-timeSeriesDataDir = "C://Users//JosephVermeil//Desktop//ActinCortexAnalysis//Data_Analysis//TimeSeriesData"
+timeSeriesDataDir = "C://Users//anumi//Desktop//ActinCortexAnalysis//Data_Analysis//TimeSeriesData"
 
 # %% Import of the experimental conditions
 
-experimentalDataDir = "C://Users//JosephVermeil//Desktop//ActinCortexAnalysis//Data_Experimental"
+experimentalDataDir = "C://Users//anumi//Desktop//ActinCortexAnalysis//Data_Experimental"
 expDf = getExperimentalConditions(experimentalDataDir, save = False)
-
 
 # %% Next manipe
 # %%%% 
 
+dates = '21.11.30'
+manips, wells, cells = 1, 1, 'all'
+depthoNames = '21.10.18_M1_M270_100X_step20'
+
+PTL, timeSeries_DF, dfLogF = mainTracker(mainDataDir, rawDataDir, depthoDir, interDataDir, 
+                                  figureDir, timeSeriesDataDir,
+                                  dates, manips, wells, cells, depthoNames, 
+                                  expDf, methodT = 'max_entropy', factorT = 0.7, 
+                                  redoAllSteps = False)
 # %% Next manipe
 # %%%%
 
@@ -112,24 +120,121 @@ PTL, timeSeries_DF, dfLogF = mainTracker(mainDataDir, rawDataDir, depthoDir, int
                                   expDf, methodT = 'max_entropy', factorT = 0.7, 
                                   redoAllSteps = False)
 
+# %% Stand alone xyz tracker: To test images from Atchoum with code
+# %%%% Test run 1 with 60x objective depthographs from Atchoum
 
+mainDataDir = 'D:/Anumita/Data'
+rawDataDir = os.path.join(mainDataDir, 'Raw')
+depthoDir = os.path.join(rawDataDir, 'EtalonnageZ')
+interDataDir = os.path.join(mainDataDir, 'Intermediate_Py')
+figureDir = os.path.join(mainDataDir, 'Figures')
+timeSeriesDataDir = "C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData"
 
+imageDir = 'D:/Anumita/Data/Raw/21.11.26'
+imageName = 'Cell1_Chamber1_15mT_50ms_15s_Stack.tif'
+#imageName =  '0-2-4umStack_M450only_60X.tif'
+imagePath = os.path.join(imageDir, imageName)
+depthoNames = '21.11.30_M450_step50_60X'
 
+cellID = 'test'
+I = io.imread(imagePath).T # trqnspose chqnge if not necessqry
+print(I.shape)
 
+manipDict = {}
+manipDict['experimentType'] = 'tracking'
+manipDict['scale pixel per um'] = 15.8*0.6
+manipDict['optical index correction'] = 1.33/1.52
+manipDict['magnetic field correction'] = 0
+manipDict['beads bright spot delta'] = 0
+manipDict['bead type'] = 'M450'
+manipDict['bead diameter'] = 4503
+manipDict['loop structure'] = '3_0_0'
+manipDict['normal field multi images'] = 3
+manipDict['multi image Z step'] = 500 #nm
+manipDict['with fluo images'] = False
 
+NB =  2
+PTL = XYZtracking(I, cellID, NB, manipDict, depthoDir, depthoNames)
 
+# %%%% Test run for 100X Atchoum 21/12/08 (Planes 500nm apart) - Worked shitty. The depthos were not well constructed
+# because of bad Kohler illumination
 
+mainDataDir = 'D:/Anumita/Data'
+rawDataDir = os.path.join(mainDataDir, 'Raw')
+depthoDir = os.path.join(rawDataDir, 'EtalonnageZ')
+interDataDir = os.path.join(mainDataDir, 'Intermediate_Py')
+figureDir = os.path.join(mainDataDir, 'Figures')
+timeSeriesDataDir = "C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData"
 
+imageDir = 'D:/Anumita/Data/Raw/21.12.08'
+imageName = 'bead2_7-8-9frames_Stack.tif'
+#imageName =  '0-2-4umStack_M450only_60X.tif'
+imagePath = os.path.join(imageDir, imageName)
+depthoNames = '21.12.08_M450_step50_100X'
 
+cellID = 'test'
+I = io.imread(imagePath).T # trqnspose chqnge if not necessqry
+print(I.shape)
 
+manipDict = {}
+manipDict['experimentType'] = 'tracking'
+manipDict['scale pixel per um'] = 15.8
+manipDict['optical index correction'] = 1.33/1.52
+manipDict['magnetic field correction'] = 0
+manipDict['beads bright spot delta'] = 0
+manipDict['bead type'] = 'M450'
+manipDict['bead diameter'] = 4503
+manipDict['loop structure'] = '3_0_0'
+manipDict['normal field multi images'] = 3
+manipDict['multi image Z step'] = 500 #nm
+manipDict['with fluo images'] = False
 
+NB =  1
+PTL = XYZtracking(I, cellID, NB, manipDict, depthoDir, depthoNames)
 
+# %%%% Test run 2 for 100X Atchoum 21/12/13 (Planes 500nm apart) - Shitty deptho.
 
+mainDataDir = 'D:/Anumita/Data'
+rawDataDir = os.path.join(mainDataDir, 'Raw')
+depthoDir = os.path.join(rawDataDir, 'EtalonnageZ')
+interDataDir = os.path.join(mainDataDir, 'Intermediate_Py')
+figureDir = os.path.join(mainDataDir, 'Figures')
+timeSeriesDataDir = "C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData"
 
+imageDir = 'D:/Anumita/Data/Raw/21.12.13'
+imageName = 'B3_3-4-5frames_Stack.tif'
+#imageName =  '0-2-4umStack_M450only_60X.tif'
+imagePath = os.path.join(imageDir, imageName)
+depthoNames = '21.12.13_M450_step20_100X'
 
+cellID = 'test'
+I = io.imread(imagePath).T # trqnspose chqnge if not necessqry
+print(I.shape)
 
+manipDict = {}
+manipDict['experimentType'] = 'tracking'
+manipDict['scale pixel per um'] = 15.8
+manipDict['optical index correction'] = 1.33/1.52
+manipDict['magnetic field correction'] = 0
+manipDict['beads bright spot delta'] = 0
+manipDict['bead type'] = 'M450'
+manipDict['bead diameter'] = 4503
+manipDict['loop structure'] = '3_0_0'
+manipDict['normal field multi images'] = 3
+manipDict['multi image Z step'] = 500 #nm
+manipDict['with fluo images'] = False
 
+NB =  1
+PTL = XYZtracking(I, cellID, NB, manipDict, depthoDir, depthoNames)
 
+# %% Test experiments from Atchoum
+# %%%% M1
+dates = '21.12.10'
+manips, wells, cells = 1, 1, 'all'
+depthoNames = '21.12.13_M450_100X_step20'
 
-
-
+PTL, timeSeries_DF, dfLogF = mainTracker(mainDataDir, rawDataDir, depthoDir, interDataDir, 
+                                  figureDir, timeSeriesDataDir,
+                                  dates, manips, wells, cells, depthoNames, 
+                                  expDf, methodT = 'max_entropy', factorT = 0.7, 
+                                  redoAllSteps = False)
