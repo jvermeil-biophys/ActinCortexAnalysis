13bf9fe14576b483aab09ca3c2fffc25130a2fbc
Anumita <anumitaj1603@gmail.com>
2022-02-18 15:27:27 +0100

    More plots

--- Code_Python/OptoPincher_Plots_AJ.py
+++ Code_Python/OptoPincher_Plots_AJ.py
@@ -9,16 +9,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+bead_dia = 4.503
 #%% Plotting alll three graphs (3D, 2D and Dz)
 
-expt = '20220203_100xoil_3t3optorhoa_4.5beads_15mT'
-folder = '22-02-03_M5_P1_C3_disc20um'
-date = '22.02.03'
+expt = '20121220_100xoil_3t3optorhoa_4.5beads_15mT'
+folder = '21-12-20_M1_P1_C3_disc20um'
+date = '21.12.20'
 
 file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder+'_PY.csv'
 data = pd.read_csv(file, sep=';')
-bead_dia = 4.503 #in um
+ #in um
 
 xyz_dist = data['D3'] - bead_dia
 xy_dist = data['D2'] - bead_dia
@@ -62,7 +62,7 @@ plt.savefig('D:/Anumita/PincherPlots/'+folder+'_DistancevTime.jpg')
 #%% Plotting just 3D graphs
 
 expt = '20220203_100xoil_3t3optorhoa_4.5beads_15mT'
-folder = '22-02-03_M5_P1_C3_disc20um'
+folder = '22-02-03_M2_P1_C2_disc20um'
 date = '22.02.03'
 
 file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder+'_PY.csv'
@@ -72,7 +72,7 @@ bead_dia = 4.503 #in um
 xyz_dist = data['D3'] - bead_dia
 xy_dist = data['D2'] - bead_dia
 dz = data['dz']
-t = (data['T']*1000)/60
+t = (data['T']*1000)
 #t = np.linspace(0,len(data['T']),len(data['T']))
 Nan_thresh = 3
 
@@ -81,11 +81,13 @@ xyz_dist[outlier] = np.nan
 xy_dist[outlier] = np.nan
 dz[outlier] = np.nan
 
+plt.style.use('dark_background')
+
 fig = plt.figure(figsize=(20,20))
 fig.suptitle(folder, fontsize=16)
-plt.rcParams.update({'font.size': 22})
+plt.rcParams.update({'font.size': 35})
 plt.ylabel('Thickness (nm)')
-plt.xlabel('Time (mins)')
+plt.xlabel('Time (secs)')
 
 plt.plot(t, xyz_dist)
 plt.title('3D Distance vs. Time')
@@ -98,20 +100,26 @@ expt1 = '20211220_100xoil_3t3optorhoa_4.5beads_15mT'
 folder1 = '21-12-20_M1_P1_C3_disc20um'
 file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder1+'_PY.csv'
 data1 = pd.read_csv(file, sep=';')
-t1 = np.linspace(0,len(data1['T']),len(data1['T']))
-xy_dist1 = data1['D2'] - bead_dia
+#t1 = np.linspace(0,len(data1['T']),len(data1['T']))
+t1 = (data1['T']*1000)
+xy_dist1 = data1['D3'] - bead_dia
 
 expt2 = '20211220_100xoil_3t3optorhoa_4.5beads_15mT'
 folder2 = '21-12-20_M2_P1_C3_disc20um'
 file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder2+'_PY.csv'
 data2 =  pd.read_csv(file, sep=';')
-t2 = np.linspace(0,len(data2['T']),len(data2['T']))
-xy_dist2 = data2['D2'] - bead_dia
+# t2 = np.linspace(0,len(data2['T']),len(data2['T']))
+t2 = (data2['T']*1000)
+xy_dist2 = data2['D3'] - bead_dia
 
-Nan_thresh = 3
+Nan_thresh1 = 3
+Nan_thresh2 = 3
 
-outlier = np.where(xy_dist2 > Nan_thresh)[0]
+outlier = np.where(xy_dist2 > Nan_thresh2)[0]
 xy_dist2[outlier] = np.nan
+
+outlier = np.where(xy_dist1 > Nan_thresh1)[0]
+xy_dist1[outlier] = np.nan
 
 # expt3 = '20211220_100xoil_3t3optorhoa_4.5beads_15mT'
 # folder3 = '21-12-20_M3_P1_C2_disc20um'
@@ -120,15 +128,183 @@ xy_dist2[outlier] = np.nan
 # t3 = np.linspace(0,len(data3['T']),len(data3['T']))
 # xy_dist3 = data3['D2'] - bead_dia
 
-plt.figure(figsize=(20,20))
-plt.rcParams.update({'font.size': 35})
-plt.title('M1-2_C3_DistancevsTime')
+plt.style.use('dark_background')
 
-plt.plot(t1, xy_dist1, label="30s")
+
+fig= plt.figure(figsize=(20,20))
+fig.suptitle(folder, fontsize=16)
+
+# right_side = fig.spines["right"]
+# right_side.set_visible(False)
+# top_side = fig.spines["top"]
+# top_side.set_visible(False)
+
+
+plt.rcParams.update({'font.size':35})
+plt.title('3D Distance vs Time')
+plt.ylabel('Thickness (nm)')
+plt.xlabel('Time (secs)')
+
+plt.plot(t1, xy_dist1, label="30s", color = 'red')
 plt.plot(t2, xy_dist2, label='60s')
 # plt.plot(t3, xy_dist3, label='90s')
 plt.legend()
 plt.show()
 plt.savefig('D:/Anumita/PincherPlots/C3_DistancevTime.jpg')
+
+# %% All curves
+
+expt1 = '20211220_100xoil_3t3optorhoa_4.5beads_15mT'
+folder1 = '21-12-20_M1_P1_C1_disc20um'
+file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder1+'_PY.csv'
+data1 = pd.read_csv(file, sep=';')
+#t1 = np.linspace(0,len(data1['T']),len(data1['T']))
+t1 = (data1['T']*1000)
+xy_dist1 = data1['D3'] - bead_dia
+
+expt2 = '20211220_100xoil_3t3optorhoa_4.5beads_15mT'
+folder2 = '21-12-20_M1_P1_C3_disc20um'
+file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder2+'_PY.csv'
+data2 =  pd.read_csv(file, sep=';')
+# t2 = np.linspace(0,len(data2['T']),len(data2['T']))
+t2 = (data2['T']*1000)
+xy_dist2 = data2['D3'] - bead_dia
+
+expt3 = '20220203_100xoil_3t3optorhoa_4.5beads_15mT'
+folder3 = '22-02-03_M4_P1_C1_disc20um'
+file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder3+'_PY.csv'
+data3 = pd.read_csv(file, sep=';')
+#t1 = np.linspace(0,len(data1['T']),len(data1['T']))
+t3 = (data3['T']*1000)
+xy_dist3 = data3['D3'] - bead_dia
+
+expt4 = '20220203_100xoil_3t3optorhoa_4.5beads_15mT'
+folder4 = '22-02-03_M3_P1_C1_disc20um'
+file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder4+'_PY.csv'
+data4 =  pd.read_csv(file, sep=';')
+# t2 = np.linspace(0,len(data2['T']),len(data2['T']))
+t4 = (data4['T']*1000)
+xy_dist4 = data4['D3'] - bead_dia
+
+expt5 = '20220203_100xoil_3t3optorhoa_4.5beads_15mT'
+folder5 = '22-02-03_M5_P1_C3_disc20um'
+file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder5+'_PY.csv'
+data5 =  pd.read_csv(file, sep=';')
+# t2 = np.linspace(0,len(data2['T']),len(data2['T']))
+t5 = (data5['T']*1000)
+xy_dist5 = data5['D3'] - bead_dia
+
+
+Nan_thresh1 = 3
+Nan_thresh2 = 3
+
+outlier = np.where(xy_dist2 > Nan_thresh2)[0]
+xy_dist2[outlier] = np.nan
+
+outlier = np.where(xy_dist1 > Nan_thresh1)[0]
+xy_dist1[outlier] = np.nan
+
+# expt3 = '20211220_100xoil_3t3optorhoa_4.5beads_15mT'
+# folder3 = '21-12-20_M3_P1_C2_disc20um'
+# file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder3+'_PY.csv'
+# data3 = pd.read_csv(file, sep=';')
+# t3 = np.linspace(0,len(data3['T']),len(data3['T']))
+# xy_dist3 = data3['D2'] - bead_dia
+
+plt.style.use('dark_background')
+
+
+fig= plt.figure(figsize=(20,20))
+# fig.suptitle(fontsize=16)
+
+# right_side = fig.spines["right"]
+# right_side.set_visible(False)
+# top_side = fig.spines["top"]
+# top_side.set_visible(False)
+
+
+plt.rcParams.update({'font.size':35})
+plt.title('3D Distance vs Time')
+plt.ylabel('Thickness (nm)')
+plt.xlabel('Time (secs)')
+
+plt.plot(t1, xy_dist1, label=folder1, color = 'red')
+plt.plot(t2, xy_dist2, label=folder2, color = 'blue')
+plt.plot(t3, xy_dist3, label=folder3, color = 'orange')
+plt.plot(t4, xy_dist4, label=folder4, color = 'pink')
+plt.plot(t5, xy_dist5, label=folder5, color='yellow')
+# plt.plot(t3, xy_dist3, label='90s')
+plt.legend()
+plt.show()
+plt.savefig('D:/Anumita/PincherPlots/All_DistancevTime.jpg')
+
+# %% 2nd eperiment curves
+
+expt3 = '20220203_100xoil_3t3optorhoa_4.5beads_15mT'
+folder3 = '22-02-03_M4_P1_C1_disc20um'
+file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder3+'_PY.csv'
+data3 = pd.read_csv(file, sep=';')
+#t1 = np.linspace(0,len(data1['T']),len(data1['T']))
+t3 = (data3['T']*1000)
+xy_dist3 = data3['D3'] - bead_dia
+
+expt4 = '20220203_100xoil_3t3optorhoa_4.5beads_15mT'
+folder4 = '22-02-03_M3_P1_C1_disc20um'
+file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder4+'_PY.csv'
+data4 =  pd.read_csv(file, sep=';')
+# t2 = np.linspace(0,len(data2['T']),len(data2['T']))
+t4 = (data4['T']*1000)
+xy_dist4 = data4['D3'] - bead_dia
+
+expt5 = '20220203_100xoil_3t3optorhoa_4.5beads_15mT'
+folder5 = '22-02-03_M5_P1_C3_disc20um'
+file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder5+'_PY.csv'
+data5 =  pd.read_csv(file, sep=';')
+# t2 = np.linspace(0,len(data2['T']),len(data2['T']))
+t5 = (data5['T']*1000)
+xy_dist5 = data5['D3'] - bead_dia
+
+
+Nan_thresh1 = 3
+Nan_thresh2 = 3
+
+outlier = np.where(xy_dist2 > Nan_thresh2)[0]
+xy_dist2[outlier] = np.nan
+
+outlier = np.where(xy_dist1 > Nan_thresh1)[0]
+xy_dist1[outlier] = np.nan
+
+# expt3 = '20211220_100xoil_3t3optorhoa_4.5beads_15mT'
+# folder3 = '21-12-20_M3_P1_C2_disc20um'
+# file = 'C:/Users/anumi/OneDrive/Desktop/ActinCortexAnalysis/Data_Analysis/TimeSeriesData/'+folder3+'_PY.csv'
+# data3 = pd.read_csv(file, sep=';')
+# t3 = np.linspace(0,len(data3['T']),len(data3['T']))
+# xy_dist3 = data3['D2'] - bead_dia
+
+plt.style.use('dark_background')
+
+
+fig= plt.figure(figsize=(20,20))
+# fig.suptitle(fontsize=16)
+
+# right_side = fig.spines["right"]
+# right_side.set_visible(False)
+# top_side = fig.spines["top"]
+# top_side.set_visible(False)
+
+
+plt.rcParams.update({'font.size':35})
+plt.title('3D Distance vs Time')
+plt.ylabel('Thickness (nm)')
+plt.xlabel('Time (secs)')
+
+
+plt.plot(t3, xy_dist3, color = 'blue', label = '10ms exp /90s freq | Riceball')
+plt.plot(t4, xy_dist4, color = 'red', label = '10ms exp /90s freq')
+plt.plot(t5, xy_dist5, color='yellow', label = '50ms exp /30s freq')
+# plt.plot(t3, xy_dist3, label='90s')
+plt.legend()
+plt.show()
+plt.savefig('D:/Anumita/PincherPlots/All2_DistancevTime.jpg')
 
 
